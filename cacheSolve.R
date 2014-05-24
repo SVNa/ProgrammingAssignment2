b225c239580d03e333d8dc0f5d6ee648ae052ec5
Sharada <vn.sharada@gmail.com>
2014-05-24 21:27:54 +0530

    Peer Assessment

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,17 @@
-##takes the matrix and checks if it exists in the cache. 
+##To get the cached values 
 
 cacheSolve <- function(x, ...) {
+##gets the inverse matrix from makeCacheMatrix
         i <- x$getinverse()
+
+##Checks if the inverse has already been computed. If yes, it prints the cached
+##inverse instead with a message
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+
+##If the inverse hasn't already been computed, it computes the inverse.
         data <- x$get()
         i <- solve(data, ...)
         x$setinverse(i)
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,15 +1,18 @@
-##Inputs the matrix. 
-##Caculates the inverse of it and caches it in i 
+##Inputs the matrix
+##Caculates the inverse of it and caches it in 'i' 
 
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         set <- function(y) {
-                x <<- y
+                x <<- y		
                 i <<- NULL
         }
         get <- function() x
+
+## finding the inverse of the matrix
         setinverse <- function(solve) i <<- solve
         getinverse <- function() i
+
         list(set = set, get = get,
              setinverse = setinverse,
              getinverse = getinverse)

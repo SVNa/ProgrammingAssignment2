ce001b5b834b728d1511ad39d679bc5fc1f4ff09
Sharada <vn.sharada@gmail.com>
2014-05-24 21:12:37 +0530

    Peer Assessment

---
+++ makeCacheMatrix.R
@@ -0,0 +1,16 @@
+##Inputs the matrix. 
+##Caculates the inverse of it and caches it in i 
+
+makeCacheMatrix <- function(x = matrix()) {
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) i <<- solve
+        getinverse <- function() i
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+}

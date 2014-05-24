4d8736caceff280d77d06456dcc8b79704ca6570
Sharada <vn.sharada@gmail.com>
2014-05-24 22:47:33 +0530

    Peer Assessment

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -5,14 +5,16 @@ makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
 
 ##Passes the matrix into 'x' with the special assignment operator <<- to cause
-##a search for to be made in this function's environment
+##a search for it to be made in this function's environment
         set <- function(y) {
                 x <<- y		
                 i <<- NULL
         }
         get <- function() x
 
-##Finding the inverse of the matrix
+##Finding the inverse of the matrix and storing it in 'i' with the special <<- 
+##operator
+##Caches the inverse through this function
         setinverse <- function(solve) i <<- solve
         getinverse <- function() i
 
